c9ff53038976449b00ff62bf6443c73495fe92ca
Osmar-Araujo <araujooa@gmail.com>
2022-04-29 17:31:32 -0300

    Ajustando a API, criando entitidade Pedido

--- api-les-security/src/main/java/com/les/apiv2/entities/Pedido.java
+++ api-les-security/src/main/java/com/les/apiv2/entities/Pedido.java
@@ -1,5 +1,28 @@
 package com.les.apiv2.entities;
 
-public class Pedido {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="orders")
+public class Pedido implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Date dataPedido;
+	private List<Produto> produtos;
 }
